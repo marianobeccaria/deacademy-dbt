cbdabdf883cdbfa9c0fed27387c318e5ec3c1fda
Mariano Beccaria <mbecc01@sent.com>
2026-01-29 15:28:59 +0000

    Orders tables using ephimeral clean_order table in between

---
+++ models/demo/final_order.sql
@@ -0,0 +1,18 @@
+{{
+    config
+    (
+        materialized='table'
+    )
+}}
+with clean_orders as
+(
+    select
+    ORDER_ID,
+    ORDER_DATE,
+    CUSTOMER_ID,
+    CUSTOMER_NAME,
+    CREATED_AT,
+    CURRENT_TIMESTAMP as INSERT_DTS
+    from {{ ref('clean_order') }}
+)
+select * from clean_orders
